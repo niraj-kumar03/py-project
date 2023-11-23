e38ac3f09eb6831713eae79175ff2f0103429626
niraj-kumar03 <151805319+niraj-kumar03@users.noreply.github.com>
2023-11-23 09:18:33 +0000

    Added Bootstrap and JINJA

--- app.py
+++ app.py
@@ -2,10 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        "id": 1,
+        "title": "Python Developer",
+        "location": "New York",
+        "salary": "Rs. 2000$"
+    },
+    {
+        "id": 2,
+        "title": "Python Lead",
+        "location": "Delhi",
+        "salary": "Rs. 5000$"
+    },
+    {
+        "id": 3,
+        "title": "FSE",
+        "location": "Remote"
+    }
+]
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html",
+                           jobs=JOBS)
 
 
 if __name__ == "__main__":
